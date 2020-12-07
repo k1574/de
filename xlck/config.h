1328aff7fcc8c0ddef4c351856e7146f17540763
k1574 <k1574@yandex.ru>
2020-12-07 09:15:50 +0500

    xlck: background color changed to grey instead of whit.

--- xlck/config.h
+++ xlck/config.h
@@ -1,11 +1,12 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
 static const char *group = "nogroup";
+static const char *clr = "#777777" ;
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#FFFFFF",   /* during input */
-	[FAILED] = "#FFFFFF",   /* wrong password */
+	[INPUT] =  "#777777",   /* during input */
+	[FAILED] = "#777777",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
