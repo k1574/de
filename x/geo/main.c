67e46e21cfc74861eb3fb89a2914234c402d6045
k1574 <k1574@yandex.ru>
2021-01-01 00:00:20 +0500

    x/geo: simplified and generalized structure.

--- x/geo/main.c
+++ x/geo/main.c
@@ -2,48 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <X11/Xlib.h>
-
-char *argv0;
+#include "../../arg.h"
+char *argv0, *fmt = "%dx%d\n" ;
 Display *dpy;
 int sw, sh, screen;
-
 void
 usage(void)
 {
-	fprintf(stderr, "Usage: %s [-w] [-h]\n", argv0);
+	fprintf(stderr, "usage: %s [-s screen] [-f format]\n", argv0);
 	exit(1);
 }
-
-void
-die(char *s, int e)
-{
-	fprintf(stderr, "%s: %s\n", argv0, s);
-	exit(e);
-}
-
 void
 main(int argc, char *argv[])
 {
 	argv0 = argv[0] ;
-	if(! (dpy = XOpenDisplay(NULL)) )
-		die("cannot open open display.", 1);
-	
+	if(! (dpy = XOpenDisplay(NULL)) ){
+		fprintf(stderr, "%s: cannot open open display.", argv0);
+		exit(1);
+	}
 	screen = DefaultScreen(dpy) ;
+	ARGBEGIN {
+	case 's' :
+		screen = atoi(EARGF(usage())) ;
+	break;
+	case 'f' :
+		fmt = EARGF(usage()) ;
+	break;
+	default:
+		usage();
+	} ARGEND ;
+	if(argc) usage() ;
 	sw = DisplayWidth(dpy, screen) ;
 	sh = DisplayHeight(dpy, screen) ;
-
-	if(argc==1){
-		printf("%dx%d\n", sw, sh);
-	}else if(argc==2){
-		if(!strcmp(argv[1], "-w"))
-			printf("%d\n", sw);
-		else if(!strcmp(argv[1], "-h"))
-			printf("%d\n", sh);
-		else
-			usage();
-	}else{
-		usage();
-	}
-
+	printf(fmt, sw, sh);
 	exit(0);
 }
+
