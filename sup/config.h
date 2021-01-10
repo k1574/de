7a97194f739af5a08515b34a61c0acf4e1192e86
k1574 <k1574@yandex.ru>
2021-01-10 19:16:53 +0500

    sup: added new launchers. Better following DRY principle.

--- sup/config.h
+++ sup/config.h
@@ -5,25 +5,28 @@
 #define CHROOT ""
 #define CHRDIR ""
 #define ENFORCE_WRITE_CHECK 0
+
+#define SYSEXE "/bin"
 static struct rule_t rules[] = {
-	/* UID, GID, SUID, SGID, alias(ONE string), path to binary executable.
+	/* UID, GID, SUID, SGID, alias(ONE string), path to SYSEXEary executable.
 	 * If path is set to "*" then it will be run from the "PATH" variable. */
-	{ UID1, GID1, SUID1, SGID1, "su", "/bin/su"},
-	{ UID1, GID1, SUID1, SGID1, "rmisverydangerous", "/bin/rm"},
-	{ UID1, GID1, SUID1, SGID1, "cp", "/bin/cp"},
-	{ UID1, GID1, SUID1, SGID1, "chmod", "/bin/chmod"},
+	{ UID1, GID1, SUID1, SGID1, SYSEXE "/su"},
+	{ UID1, GID1, SUID1, SGID1, "rmisverydangerous", SYSEXE "/rm"},
+	{ UID1, GID1, SUID1, SGID1, "cp", SYSEXE "/cp"},
+	{ UID1, GID1, SUID1, SGID1, "ln", SYSEXE "/ln"},
+	{ UID1, GID1, SUID1, SGID1, "chmod", SYSEXE "/chmod"},
 	/* Network utilites. */
-	{ UID1, GID1, SUID1, SGID1, "ifconfig", "/bin/ifconfig" },
+	{ UID1, GID1, SUID1, SGID1, "ifconfig", SYSEXE "/ifconfig" },
 	/* Voidlinux. */
-	{ UID1, GID1, SUID1, SGID1, "xi", "/bin/xbps-install" },
-	{ UID1, GID1, SUID1, SGID1, "xr", "/bin/xbps-remove" },
-	{ UID1, GID1, SUID1, SGID1, "sv", "/bin/sv" },
+	{ UID1, GID1, SUID1, SGID1, "xi", SYSEXE "/xbps-install" },
+	{ UID1, GID1, SUID1, SGID1, "xr", SYSEXE "/xbps-remove" },
+	{ UID1, GID1, SUID1, SGID1, "sv", SYSEXE "/sv" },
 	/* Python. */
-	{ UID1, GID1, SUID1, SGID1, "pip3", "/bin/pip3" },
-	{ UID1, GID1, SUID1, SGID1, "pip2", "/bin/pip2" },
+	{ UID1, GID1, SUID1, SGID1, "pip3", SYSEXE "/pip3" },
+	{ UID1, GID1, SUID1, SGID1, "pip2", SYSEXE "/pip2" },
 	/* Archlinux package manager. */
-	{ UID1, GID1, SUID1, SGID1, "pacman", "/bin/pacman"},
+	{ UID1, GID1, SUID1, SGID1, "pacman", SYSEXE "/pacman"},
 	/* System utils. */
-	{ UID1, GID1, SUID1, SGID1, "shutdown", "/bin/shutdown"},
+	{ UID1, GID1, SUID1, SGID1, "shutdown", SYSEXE "/shutdown"},
 	{ 0 },
 };
